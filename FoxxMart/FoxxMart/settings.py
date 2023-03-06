03c84948b51ca4548605ad52d7203d193ef4ed09
Mufudzi Christopher <mnhamoinesu@gmail.com>
2023-03-06 13:37:06 +0200

    Marz Products

--- FoxxMart/FoxxMart/settings.py
+++ FoxxMart/FoxxMart/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'Projects',
     'Marz',
 
+    'django_browser_reload',
     'crispy_forms',
     'widget_tweaks',
     'taggit',
@@ -65,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
+
 
 ]
 
--- FoxxMart/FoxxMart/urls.py
+++ FoxxMart/FoxxMart/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"), name="password_reset_complete"),
-    # path('__debug__/', include(debug_toolbar.urls))
+    path("__reload__/", include("django_browser_reload.urls")),
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
